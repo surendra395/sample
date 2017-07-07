6a1c258cb4bf0cade0064c264cf21347e405a953
surendra395 <suri.suri395@gmail.com>
2017-07-07 12:24:47 +0530

    commit message

---
+++ abc.java
@@ -0,0 +1,16 @@
+class test
+{
+ int a=3;
+  void show()
+{
+system.out.println("value of a "+a);
+system.out.println("value of a "+b);
+ }
+public static void main(String args[])
+{
+test object1=new test();
+object.show();
+}
+}
+
+
